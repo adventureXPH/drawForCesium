ec32b106c5c917a3331a15bc125ce2666b24941a
adventureXPH <315843040@qq.com>
2018-06-22 07:08:06 +0800

    drawForCesium第一版(First edition)
    说明：
          此项目是以drawHelper插件为原型进行了二次开发
    增加了一些算法，解决了之前插件的一些原生BUG，优
    化了功能，抽离了算法和事件代码，方便后续增加算法
    二次开发。
    
    功能：
           为cesium框架增加动态标绘功能
    Explanation:
           This project is a secondary development using the drawHelper plugin as a prototype
    Added some algorithms to solve some of the original plug-in BUG, excellent
    The function is removed, the algorithm and event code are extracted, and it is convenient to add subsequent algorithms
    Secondary development.
    
    Features:
            Adding dynamic plotting capabilities for the cesium framework
    联系方式：
         2500599598@qq.com

---
+++ js/DrawWorker/draw.js
@@ -0,0 +1,124 @@
+var center = [110.98, 30.83];
+var timer = null;
+var viewer = null;
+var scene = null;
+var canvas = null;
+var clock = null;
+var camera = null;
+
+$(function () {
+    $(document).ready(function () {
+        initialGlobeView();
+        initDrawHelper();
+    });
+    
+    function initialGlobeView() {
+	    viewer = new Cesium.Viewer('cesiumContainer', {
+            imageryProvider : Cesium.createTileMapServiceImageryProvider({
+        			url : Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+    		}),
+	        selectionIndicator: true
+	    });	    
+    }
+    
+    function initDrawHelper() {
+    	$("#toolbar").html("");
+        $("#loggingText").html("");
+        var drawHelper = new DrawHelper(viewer);
+        var scene = viewer.scene;
+        var toolbar = drawHelper.addToolbar(document.getElementById("toolbar"), {
+            buttons: ['marker', 'polyline', 'polygon', 'circle', 'extent',"tailedAttackArrow"]
+        });
+        toolbar.addListener('markerCreated', function (event) {
+            loggingMessage('Marker created at ' + event.position.toString());
+            // create one common billboard collection for all billboards
+            var b = new Cesium.BillboardCollection();
+            scene.primitives.add(b);
+            var billboard = b.add({
+                show: true,
+                position: event.position,
+                pixelOffset: new Cesium.Cartesian2(0, 0),
+                eyeOffset: new Cesium.Cartesian3(0.0, 0.0, 0.0),
+                horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+                verticalOrigin: Cesium.VerticalOrigin.CENTER,
+                scale: 1.0,
+                image: './images/glyphicons_242_google_maps.png',
+                color: new Cesium.Color(1.0, 1.0, 1.0, 1.0)
+            });
+            billboard.setEditable();
+        });
+        toolbar.addListener('polylineCreated', function (event) {
+            loggingMessage('Polyline created with ' + event.positions.length + ' points');
+            var polyline = new DrawHelper.PolylinePrimitive({
+                positions: event.positions,
+                width: 5,
+                geodesic: true
+            });
+            scene.primitives.add(polyline);
+            polyline.setEditable();
+            polyline.addListener('onEdited', function (event) {
+                loggingMessage('Polyline edited, ' + event.positions.length + ' points');
+            });
+
+        });
+        toolbar.addListener('polygonCreated', function (event) {
+            loggingMessage('钳击箭头');
+            var polygon = new DrawHelper.PolygonPrimitive({
+                positions: event.positions,
+                custom:event.custom,
+                material: Cesium.Material.fromType(Cesium.Material.ColorType)
+            });
+            scene.primitives.add(polygon);
+            polygon.setEditable();
+            polygon.addListener('onEdited', function (event) {
+            	loggingMessage('钳击箭头');
+            });
+
+        });
+        toolbar.addListener('tailedAttackCreated', function (event) {
+            loggingMessage('攻击箭头');
+            var polygon = new DrawHelper.TailedAttackPrimitive({
+                positions: event.positions,
+                custom:event.custom,
+                material: Cesium.Material.fromType(Cesium.Material.ColorType)
+            });
+            scene.primitives.add(polygon);
+            polygon.setEditable();
+            polygon.addListener('onEdited', function (event) {
+            	loggingMessage('攻击箭头');
+            });
+
+        });
+        toolbar.addListener('circleCreated', function (event) {
+            loggingMessage('Circle created: center is ' + event.center.toString() + ' and radius is ' + event.radius.toFixed(1) + ' meters');
+            var circle = new DrawHelper.CirclePrimitive({
+                center: event.center,
+                radius: event.radius,
+                material: Cesium.Material.fromType(Cesium.Material.RimLightingType)
+            });
+            scene.primitives.add(circle);
+            circle.setEditable();
+            circle.addListener('onEdited', function (event) {
+                loggingMessage('Circle edited: radius is ' + event.radius.toFixed(1) + ' meters');
+            });
+        });
+        toolbar.addListener('extentCreated', function (event) {
+            var extent = event.extent;
+            loggingMessage('箭头创建');
+            var extentPrimitive = new DrawHelper.ExtentPrimitive({
+                extent: extent,
+                material: Cesium.Material.fromType(Cesium.Material.ColorType)
+            });
+            scene.primitives.add(extentPrimitive);
+            extentPrimitive.setEditable();
+            extentPrimitive.addListener('onEdited', function (event) {
+            	 loggingMessage('箭头创建');
+            });
+        });
+
+        var logging = document.getElementById('loggingText');
+        function loggingMessage(message) {
+            logging.innerHTML = message;
+        }
+    }
+});
